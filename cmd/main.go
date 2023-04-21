0785408e01ec04d86cc782fdb367d3254c2b9d8c
Beksultan15 <besk03@mail.ru>
2023-04-21 17:27:50 +0600

    all work

--- cmd/main.go
+++ cmd/main.go
@@ -42,7 +42,7 @@ func main() {
 }
 
 func initConfig()  error {
-	viper.AddConfigPath("./configs")
+	viper.AddConfigPath("../configs")
 	viper.SetConfigName("config")
 	return viper.ReadInConfig()
 }
